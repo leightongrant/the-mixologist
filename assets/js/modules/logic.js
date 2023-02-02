05de6e3e2917e1c9fb19af460c5180021636114d
Leighton Grant <oneal.grant@outlook.com>
2023-02-02 14:16:51 +0000

    Adds some functions to make api calls

--- assets/js/modules/logic.js
+++ assets/js/modules/logic.js
@@ -1,10 +1,93 @@
-import { appid2 } from "./keys.js";
-
-const testFunction = () => console.log(appid2);
+import { appid1, appid2 } from "./apikeys.js";
 
 
+// This function should query the cocktaiĺ data base and create an object with
+// all required data
+const getCocktail = (search) => {
+    const apiURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
+    fetch(apiURL)
+        .then((response) => {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+};
+
+
+// This function should query the wikipedia api and get a description of the cocktail
+const getCocktailDescription = (search) => {
+    const apiURL = `https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&utf8=&format=json&srsearch=${search}`;
+    fetch(apiURL)
+        .then((response) => {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+};
+
+
+// This function should request a location from the browser
+const getNavigatorLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        console.log(lat, lon);
+
+    }, err => {
+        console.log(err);
+    });
+
+};
+
+
+
+// This function should query openweathermap api for a city name to get lat and lon data
+const getLocation = (city, appid) => {
+    const apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=5&appid=${appid}`;
+    fetch(apiURL)
+        .then((response) => {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+};
+
+// This function should query the local business api and create an object with required data
+const getLocalBars = (search) => {
+    const apiURL = `https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&utf8=&format=json&srsearch=${search}`;
+    fetch(apiURL)
+        .then((response) => {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+};
 
 
 
 
-export { testFunction };
+
+
+
+export { getNavigatorLocation };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-import { testFunction } from "./modules/logic.js";
+import { getNavigatorLocation } from "./modules/logic.js";
 
 $(function () {
-    testFunction();
+    if (navigator.geolocation) {
+        getNavigatorLocation();
+    }
 
 
 
