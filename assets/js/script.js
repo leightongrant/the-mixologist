dd2ccfb63e588466f844e654740e3c49ec551fe4
Leighton Grant <oneal.grant@outlook.com>
2023-02-08 16:26:15 +0000

    Adds functions to recommend cocktails based on user preferences

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-import { getCocktail, getCocktailDescription, getRandomCocktails, main } from "./modules/logic.js";
+import { getRandomCocktails, main, getRecommendations, saveIngredients } from "./modules/logic.js";
 
 
 $(function () {
@@ -35,6 +35,26 @@ $(function () {
         e.stopPropagation();
         main($(e.target).parent()[0].children[0].innerText);
     });
+
+
+    // Settings
+    $('#ingOneOp1').attr('checked', true);
+    $('#ingTwoOp1').attr('checked', true);
+    $('#settings-form').on('submit', (e) => {
+        e.preventDefault();
+        let ingOne = $('input[type="radio"][name="ingOneOp"]:checked').val();
+        let ingTwo = $('input[type="radio"][name="ingTwoOp"]:checked').val();
+        saveIngredients(ingOne, ingTwo);
+        $('#settings').addClass('hide');
+    });
+
+    $('#settings-button').on('click', () => {
+        $('#settings').addClass('show');
+    });
+
+    // Recommended cocktails
+    getRecommendations();
+
 
 
 
